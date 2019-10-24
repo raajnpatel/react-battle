9e8d04a783b108bf072f922e7b0a33834899fe7d
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-24 11:24:07 -0700

    added state to navbar for color changing

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 export default class Popular extends React.Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedLanguage: "All"
+        };
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    updateLanguage(selectedLanguage){
+        this.setState({
+            selectedLanguage
+        })
+    }
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
@@ -8,7 +22,10 @@ export default class Popular extends React.Component{
             <ul className = 'flex-center'>
                 {languages.map((language)=>(
                     <li key={language}>
-                        <button className="btn-clear nav-link">
+                        <button
+                            onClick={()=> this.updateLanguage(language)}
+                            style={language===this.state.selectedLanguage ? {color: 'red'} : null}
+                            className="btn-clear nav-link">
                             {language}
                         </button>
                     </li>
